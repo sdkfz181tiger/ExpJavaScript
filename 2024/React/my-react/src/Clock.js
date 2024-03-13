2234487a96879f022140c5a703936549e5f3ade9
Kajiru <sdkfz181tiger@gmail.com>
2024-03-13 15:35:26 +0900

    Update

--- 2024/React/my-react/src/Clock.js
+++ 2024/React/my-react/src/Clock.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './Clock.css';
+import React from "react"
+import logo from "./logo.svg";
+import "./Clock.css";
 
-function Clock() {
-  return (
-    <div className="Clock">
-      <p>Time:{(new Date()).toLocaleString()}</p>
-    </div>
-  );
+class Clock extends React.Component{
+  render(){
+    return(
+        <div className="Clock">
+          <p className="Clock-View">Time:{(new Date()).toLocaleString()}</p>
+        </div>
+      );
+  }
 }
 
 export default Clock;
