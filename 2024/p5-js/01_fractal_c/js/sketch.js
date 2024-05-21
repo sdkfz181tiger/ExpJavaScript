ab47f4a0544ced11927967f965ebbc33d63b3104
Kajiru <sdkfz181tiger@gmail.com>
2024-05-21 23:39:16 +0900

    Update

--- 2024/p5-js/01_fractal_c/js/sketch.js
+++ 2024/p5-js/01_fractal_c/js/sketch.js
@@ -17,12 +17,12 @@ function draw(){
 
 	stroke(255); strokeWeight(1);
 
-	const sX = width / 4;
-	const sY = height / 2;
-	const eX = width / 4 * 3;
-	const eY = sY;
+	const aX = width * 0.3;
+	const aY = height * 0.25;
+	const bX = width * 0.7;
+	const bY = height * 0.25;
 	const cCurve = new CCurve();
-	cCurve.draw(sX, sY, eX, eY, 2);
+	cCurve.draw(aX, aY, bX, bY, 14);
 }
 
 function getColor(colors){
@@ -35,34 +35,19 @@ class CCurve{
 		console.log("CCurve");
 	}
 
-	draw(sX, sY, eX, eY, depth){
-		//line(sX, sY, eX, eY);
-		this.splitLine(sX, sY, eX, eY, depth);
-	}
-
-	splitLine(sX, sY, eX, eY, depth){
-
+	draw(aX, aY, bX, bY, depth){
+		
 		if(depth <= 0){
-			line(sX, sY, eX, eY);
+			line(aX, aY, bX, bY);
 			return;
 		}
-
-		const mX = (sX + eX) / 2;
-		const mY = (sY + eY) / 2;
-		const len = sqrt((eX-sX)**2+(eY-sY)**2) / 2;
-
-		circle(mX, mY, 5);
-		// Left
-		this.splitLine(sX, sY, mX, mY, depth-1);
-		const degL = atan2(mY-sY, mX-sX);
-		const lX = sX + len * cos(degL-90);
-		const lY = sY + len * sin(degL-90);
-		this.splitLine(sX, sY, lX, lY, depth-1);
-		// Right
-		this.splitLine(mX, mY, eX, eY, depth-1);
-		const degR = atan2(mY-eY, mX-eX);
-		const rX = eX + len * cos(degR+90);
-		const rY = eY + len * sin(degR+90);
-		this.splitLine(eX, eY, rX, rY, depth-1);
+		const mX = (aX + bX) / 2;
+		const mY = (aY + bY) / 2;
+		const len = sqrt((bX-aX)**2+(bY-aY)**2) / 2;
+		const deg = atan2(bY-aY, bX-aX) + 90;
+		const cX = mX + len * cos(deg);
+		const cY = mY + len * sin(deg);
+		this.draw(aX, aY, cX, cY, depth-1);
+		this.draw(cX, cY, bX, bY, depth-1);
 	}
 }
