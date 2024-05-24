59e6311c9e373bf478ea3a09cb32ececeba8dbf2
Kajiru <sdkfz181tiger@gmail.com>
2024-05-24 14:22:46 +0900

    Update

--- 2024/React/03_cdn_bmi/main.js
+++ 2024/React/03_cdn_bmi/main.js
@@ -1,9 +1,10 @@
 "use strict"
 
-const names = [
-	"メタルスライム",
-	"はぐれメタル",
-	"メタルキング"
+const results = [
+	"やせ",
+	"標準",
+	"軽度肥満",
+	"重度肥満"
 ];
 
 // Title
@@ -14,16 +15,33 @@ const MyTitle = ({myTitle})=>{return(
 );}
 
 // BMI Calc
-const MyBMICalc = ({init})=>{
+const MyBMICalc = ({init, myResults})=>{
+
+	const [form, setForm] = React.useState({// State
+		cm: 170,
+		kg: 60
+	});
+	const handleForm = e=>{
+		setForm({...form, [e.target.name]: e.target.value});
+	}
+
 	const [result, setResult] = React.useState(init);// State
 	const clickEvent =()=>{
-		console.log("Click!!");
-		setResult("Hogehoge");
+		// Calc
+		const bmi = Number(form.kg) / ((Number(form.cm) * 0.01)**2);
+		let result = "---";
+		if(bmi < 18.5) result = myResults[0];
+		else if(bmi < 25.0) result = myResults[1];
+		else if(bmi < 30.0) result = myResults[2];
+		else result = myResults[3];
+		setResult(result);// Result
 	}
 	return(
 	<div>
-		<p>身長(cm): <input name="cm" type="text" value="170"/></p>
-		<p>体重(kg): <input name="kg" type="text" value="60"/></p>
+		<p>身長(cm): <input id="cm" name="cm" type="text"
+			onChange={handleForm} value={form.cm}/></p>
+		<p>体重(kg): <input id="kg" name="kg" type="text"
+			onChange={handleForm} value={form.kg}/></p>
 		<p><button onClick={clickEvent}>計算</button></p>
 		<p>結果: {result}</p>
 	</div>
@@ -34,6 +52,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<MyTitle myTitle="BMI Calc" />
-		<MyBMICalc init="---" />
+		<MyBMICalc init="---" myResults={results} />
 	</React.StrictMode>
 );
