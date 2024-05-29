69753bfaa89ffc9acdba504052c4da611ff4d2b5
Kajiru <sdkfz181tiger@gmail.com>
2024-05-30 07:33:17 +0900

    Update

--- 2024/p5-js/03_SierpinskiCarpet /js/sketch.js
+++ 2024/p5-js/03_SierpinskiCarpet /js/sketch.js
@@ -12,26 +12,33 @@ function setup(){
 function draw(){
 	background(BLACK);
 
+	// 四角形の辺の長さと座標
 	const len = (width<height)?width*0.8:height*0.8;
 	const x = width/2 - len/2;
 	const y = height/2 - len/2;
-	drawFractal(x, y, len, 3);
+	fill(WHITE);
+	square(x, y, len);// 最初の四角形を描く
+	fill(BLACK);
+	drawFractal(x, y, len, 3);// 再帰処理の開始
 }
 
 function drawFractal(x, y, len, depth){
-	fill(WHITE);
-	square(x, y, len);
-	fill(BLACK);
+
+	// 四角形の辺を3等分する
 	const pad = len / 3;
+	// 9x9の四角形の座標を求める
 	for(let r=0; r<3; r++){
 		for(let c=0; c<3; c++){
+			// 分割された四角形の座標
 			const cX = x + pad*c;
 			const cY = y + pad*r;
+			// 中央の四角形であれば四角形を描画
 			if(r==1 && c==1){
 				square(cX, cY, pad);
 				continue;
 			}
-			if(depth <= 0) continue;
+			if(depth <= 0) continue;// これ以上再帰をしない
+			// その他の8つの四角形に対して再帰
 			drawFractal(cX, cY, pad, depth-1);
 		}
 	}
--- 2024/p5-js/04_SierpinskiCurve/js/sketch.js
+++ 2024/p5-js/04_SierpinskiCurve/js/sketch.js
@@ -15,27 +15,28 @@ function draw(){
 }
 
 function drawFractal(x, y){
-	const depth = 5;
-	const cmds = createCmds("AA", depth);
+	const depth = 5;// 再帰処理をする回数
+	const cmds = createCmds("AA", depth);// 再帰処理の開始
 	const len = ((width<height)? width:height)/100;
-	drawCmds(width/2, 0, cmds, len);
+	drawCmds(width/2, 0, cmds, len);// コマンドを元に図形を描画
 }
 
 function createCmds(str, depth){
 	if(depth <= 0) return str;
 	let result = "";
 	for(let s of str){
-		if(s == "A") result += "BABA";
-		if(s == "B") result += "BABB";
+		if(s == "A") result += "BABA";// Aを置き換える
+		if(s == "B") result += "BABB";// Bを置き換える
 	}
-	return createCmds(result, depth-1);
+	return createCmds(result, depth-1);// 再帰処理
 }
 
 function drawCmds(x, y, cmds, len){
+	push();
 	translate(x, y);
 	rotate(90);
 	for(let cmd of cmds){
-		if(cmd == "A"){
+		if(cmd == "A"){// "A"の描画命令
 			line(0, 0, len, 0);
 			translate(len, 0);
 			rotate(90);
@@ -44,7 +45,7 @@ function drawCmds(x, y, cmds, len){
 			rotate(90);
 			continue;
 		}
-		if(cmd == "B"){
+		if(cmd == "B"){// "B"の描画命令
 			line(0, 0, len, 0);
 			translate(len, 0);
 			rotate(-45);
@@ -54,4 +55,5 @@ function drawCmds(x, y, cmds, len){
 			continue;
 		}
 	}
+	pop();
 }
