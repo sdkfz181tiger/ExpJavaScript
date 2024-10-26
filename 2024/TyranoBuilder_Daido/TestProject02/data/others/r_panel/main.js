5da13d8bf9d203c453e8dc5734d757de02210104
Kajiru <sdkfz181tiger@gmail.com>
2024-10-26 12:20:29 +0900

    Update

--- 2024/TyranoBuilder_Daido/TestProject02/data/others/r_panel/main.js
+++ 2024/TyranoBuilder_Daido/TestProject02/data/others/r_panel/main.js
@@ -1,5 +1,10 @@
 console.log("ResultPanel");
 
+// システム変数等にアクセスする
+// TYRANO.kag.variable.f // ゲーム変数
+// TYRANO.kag.variable.tf// 一時変数
+// TYRANO.kag.variable.sf// システム変数
+
 // Panel
 const rp_body  = $('body');
 const rp_panel = $('<div class="rp_panel"></div>');
@@ -9,6 +14,7 @@ rp_body.append(rp_panel);
 let rp_toggle_flg = false;
 const rp_toggle = $('<button class="rp_toggle">OPEN</button>');
 rp_toggle.on("click", ()=>{
+	refleshPanel();// Reflesh
 	if(!rp_toggle_flg){
 		rp_panel.addClass("rp_panel_open");
 		rp_panel.removeClass("rp_panel_close");
@@ -21,3 +27,21 @@ rp_toggle.on("click", ()=>{
 	rp_toggle_flg = !rp_toggle_flg;
 });
 rp_panel.append(rp_toggle);
+
+// Big5
+const rp_title = $('<h1 class="rp_title">=5つの性格傾向=</h1>');
+rp_panel.append(rp_title);
+const rp_ul = $('<ul class="rp_ul"></ul>');
+rp_panel.append(rp_ul);
+function refleshPanel(){
+	rp_ul.empty();
+	const keys = TYRANO.kag.variable.sf.big5_keys;
+	const nums = TYRANO.kag.variable.sf.big5_nums;
+	for(let i=0; i<keys.length; i++){
+		const key = keys[i];
+		const num = nums[i];
+		const rp_li = $('<li></li>');
+		rp_li.append(key + ":" + num + "%");
+		rp_ul.append(rp_li);
+	}
+}
--- 2024/TyranoBuilder_Daido/TestProject02/data/scenario/scene1.ks
+++ 2024/TyranoBuilder_Daido/TestProject02/data/scenario/scene1.ks
@@ -4,18 +4,20 @@
 [iscript]
 // 分析の最終結果を格納する配列
 // この配列の値に加算、減算するよ!!
-sf.forces = [30, 20, 80, 50, 0];
+sf.big5_keys = ["外交性", "協調性", "勤勉性", "情動性", "創造性"];
+sf.big5_nums = [10, 20, 30, 40, 50];
 [endscript]
 
 [bg  storage="room.jpg"  time="1000"  ]
-[tb_show_message_window  ]
-[tb_start_text mode=1 ]
-分析結果を表示します。[p]
-[_tb_end_text]
-
 [tb_start_tyrano_code]
 [loadcss file="./data/others/r_panel/main.css"]
 [loadjs storage="./r_panel/main.js"]
 [_tb_end_tyrano_code]
+
+[tb_show_message_window  ]
+[tb_start_text mode=1 ]
+分析結果を確認するパネルを作りました。[p]
+どうかしら?[p]
+[_tb_end_text]
 
 [s  ]
