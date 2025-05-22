bda9d765045650e86c3ed8e528912828ac32d0bb
Kajiru <sdkfz181tiger@gmail.com>
2025-05-23 02:48:50 +0900

    Update

---
+++ 2025/PixiJS/02/main.js
@@ -0,0 +1,30 @@
+console.log("main.js");
+
+const W = 320;
+const H = 240;
+const cX = W / 2;
+const cY = H / 2;
+const imgPiyo = "images/piyo01.png";
+
+const app = new PIXI.Application();
+await app.init({ width: W, height: H });
+document.body.appendChild(app.canvas);
+
+await PIXI.Assets.load(imgPiyo);
+const sprite = PIXI.Sprite.from(imgPiyo);
+sprite.x = cX;
+sprite.y = cY;
+sprite.anchor.set(0.5);
+app.stage.addChild(sprite);
+
+// 拡大,縮小するサンプル
+let scaleDirection = 1; // 拡大・縮小の方向
+app.ticker.add(() => {
+    sprite.scale.x += 0.01 * scaleDirection;
+    sprite.scale.y += 0.01 * scaleDirection;
+
+    // 拡大・縮小の切り替え
+    if (sprite.scale.x > 1.5 || sprite.scale.x < 0.5) {
+        scaleDirection *= -1;
+    }
+});
---
+++ 2025/PixiJS/03/main.js
@@ -0,0 +1,33 @@
+console.log("main.js");
+
+const W = 320;
+const H = 240;
+const cX = W / 2;
+const cY = H / 2;
+const imgPiyo = "images/piyo01.png";
+
+const app = new PIXI.Application();
+await app.init({ width: W, height: H });
+document.body.appendChild(app.canvas);
+
+await PIXI.Assets.load(imgPiyo);
+const sprite = PIXI.Sprite.from(imgPiyo);
+sprite.x = cX;
+sprite.y = cY;
+sprite.anchor.set(0.5);
+app.stage.addChild(sprite);
+
+// スプライトをクリックしたときに色をランダムに変更
+sprite.interactive = true;
+sprite.buttonMode = true;
+sprite.on("pointerdown", () => {
+    sprite.tint = Math.random() * 0xFFFFFF; // ランダムな色を設定
+});
+
+// 円運動するサンプル
+let elapsed = 0.0;
+app.ticker.add((ticker) => {
+    elapsed += ticker.deltaTime;
+    sprite.x = cX + Math.cos(elapsed / 30.0) * 80.0;
+    sprite.y = cY + Math.sin(elapsed / 50.0) * 80.0;
+});
